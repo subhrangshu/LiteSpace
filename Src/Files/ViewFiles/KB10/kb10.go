512bcea7502d758fd43403b62a86aca05aa3523b
subhrangshu <lalshu.rnj@gmail.com>
2021-01-05 22:21:06 +0530

    In-Memory

--- Src/Files/ViewFiles/KB10/kb10.go
+++ Src/Files/ViewFiles/KB10/kb10.go
@@ -1,19 +1,18 @@
 package KB10
 
 import (
-	"LiteSpace/Src/Shared"
 	"database/sql"
 	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/gofiber/fiber/v2"
-	"io/ioutil"
+	"net/http"
 )
 
 func KB10(fibSess *fiber.App, sqlSess *sql.DB, redSess *redis.Client, router fiber.Router) error {
 	router.Get("/10kb", func(ctx *fiber.Ctx) error {
 		if ctx.Query("id") != "" {
-			// _,_ = http.Get("localhost:3000/10KB-" + ctx.Query("id") + ".txt")
-			_, _ = ioutil.ReadFile(Shared.FileTestAddress + "10KB-" + ctx.Query("id") + ".txt")
+			_, _ = http.Get("localhost:3000/10KB-" + ctx.Query("id") + ".txt")
+			// _, _ = ioutil.ReadFile(Shared.FileTestAddress + "10KB-" + ctx.Query("id") + ".txt")
 			return ctx.SendString("Viewed: " + "10KB-" + ctx.Query("id"))
 		}
 		fmt.Print("ID Not Provided")
--- Src/Files/ViewFiles/KB1000/kb1000.go
+++ Src/Files/ViewFiles/KB1000/kb1000.go
@@ -1,19 +1,18 @@
 package KB1000
 
 import (
-	"LiteSpace/Src/Shared"
 	"database/sql"
 	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/gofiber/fiber/v2"
-	"io/ioutil"
+	"net/http"
 )
 
 func KB1000(fibSess *fiber.App, sqlSess *sql.DB, redSess *redis.Client, router fiber.Router) error {
 	router.Get("/1000kb", func(ctx *fiber.Ctx) error {
 		if ctx.Query("id") != "" {
-			// _,_ = http.Get("localhost:3000/1000KB-" + ctx.Query("id") + ".txt")
-			_, _ = ioutil.ReadFile(Shared.FileTestAddress + "1000KB-" + ctx.Query("id") + ".txt")
+			_, _ = http.Get("localhost:3000/1000KB-" + ctx.Query("id") + ".txt")
+			// _, _ = ioutil.ReadFile(Shared.FileTestAddress + "1000KB-" + ctx.Query("id") + ".txt")
 			return ctx.SendString("Viewed: " + "1000KB-" + ctx.Query("id"))
 		}
 		fmt.Print("ID Not Provided")
