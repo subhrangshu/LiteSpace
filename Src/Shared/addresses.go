5df8bc53a8705ef4a278c6c625b6c43989e5115b
subhrangshu <lalshu.rnj@gmail.com>
2021-01-05 18:17:47 +0530

    Prepared for final test

--- Src/Shared/addresses.go
+++ Src/Shared/addresses.go
@@ -6,7 +6,7 @@ package Shared
 // const FileTestAddress string = "/mnt/glusterfs/"
 
 //Server
-const FileBaseAddress string = "/mnt/1F8376FA6D76E846/Documents/Research Documents/More/Bigdata/"
-const FileTestAddress string = "/mnt/1F8376FA6D76E846/Documents/Research Documents/More/Bigdata/Tests/"
+const FileBaseAddress string = "/mnt/volume_nyc3_01/testfiles/"
+const FileTestAddress string = "/mnt/volume_nyc3_01/litespace/"
 
 // const FileTestAddress string = "/mnt/glusterfs/"
