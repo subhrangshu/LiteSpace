3e8b10779e7a08c644edfe0a3963d94ad9efc376
subhrangshu <lalshu.rnj@gmail.com>
2021-01-05 12:47:23 +0530

    Added Load Testing Files

---
+++ LoadTesting/locustfile.py
@@ -0,0 +1,10 @@
+from locust import HttpUser, between, task
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(5, 15)
+
+    @task
+    def data(self):
+        for i in range(10):
+            self.client.get("/?id="+str(i))
